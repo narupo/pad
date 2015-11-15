b1968003c60cff6e2bffa0ef558c99238d3d791c
narupo <narupo.glab@gmail.com>
2015-11-15 16:05:00 +0900

    file

--- src/config.c
+++ src/config.c
@@ -21,23 +21,44 @@ config_delete(Config* self) {
     }
 }
 
+bool
+config_load_settings(Config* self) {
+    //! Create cap's config directory at user's home
+    char const* dirpath = buffer_getc(self->config_dirpath);
+
+    if (!file_is_exists(dirpath)) {
+        if (file_mkdir(dirpath, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
+            return false;
+    }
+    return true;
+}
+
 Config*
 config_new(void) {
     Config* self = (Config*) calloc(1, sizeof(Config));
     if (!self)
         goto fail_0;
 
-    // Make dir path
+    //! Make Buffers
+
+    //! Make cap's config directory path
     self->config_dirpath = buffer_new_str(config_dirpath);
     if (!self->config_dirpath)
         goto fail_1;
 
+    //! Make cap's source directory path
     self->source_dirpath = buffer_new_str(source_dirpath);
     if (!self->source_dirpath)
         goto fail_2;
 
+    //! Load settings from files
+    if (!config_load_settings(self))
+        goto fail_3;
+
     return self;
 
+fail_3:
+    buffer_delete(self->source_dirpath);
 fail_2:
     buffer_delete(self->config_dirpath);
 fail_1:
@@ -47,7 +68,7 @@ fail_0:
 }
 
 char*
-config_make_file_path(Config const* self, char const* name) {
+config_make_path_from_base(Config const* self, char const* name) {
     if (!name)
         goto fail_0;
 
