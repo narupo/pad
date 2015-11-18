86f64753985b2e8cbfe51f703c34fb4ca4ffdc3c
narupo <narupo.glab@gmail.com>
2015-11-18 20:25:05 +0900

    Comment

--- src/buffer.h
+++ src/buffer.h
@@ -9,15 +9,8 @@
 #include <stdbool.h>
 
 /**
- * Desc.
- *
- * @param[in] self
- */
-void
-buffer_delete(Buffer* self);
-
-/**
- * Desc.
+ * Safe destruct buffer and clear pointer value to NULL.
+ * If self is null, do not anything.
  *
  * @param[in] self
  */
@@ -25,89 +18,109 @@ void
 buffer_safe_delete(Buffer** self);
 
 /**
- * Desc.
+ * Construct buffer.
  *
- * @return
+ * @return Success to pointer to buffer.
+ * @return Failed to NULL.
  */
 Buffer*
 buffer_new(void);
 
 /**
- * Desc.
+ * Construct buffer from string.
  *
- * @param[in] 
- * @return	  
+ * @param[in] src Source string.
+ * @return        Success to pointer to buffer.
+ * @return        Failed to NULL.
  */
 Buffer*
 buffer_new_str(char const* src);
 
 /**
- * Desc.
+ * Construct buffer from number of size.
  *
- * @param[in] 
- * @return	  
+ * @param[in] size Allocate memory size.
+ * @return	       Success to pointer to buffer.
+ * @return         Failed to NULL.
  */
 Buffer*
 buffer_new_size(size_t size);
 
 /**
+ * Get used length of buffer.
  *
+ * @param[in] self 
+ * @return         Number of length.
  */
 size_t
 buffer_length(Buffer const* self);
 
 /**
- * Desc.
+ * Get pointer to bytes in buffer.
  *
- * @param[in] 
- * @return	  
+ * @param[in] self 
+ * @return         Pointer to bytes.
  */
 char const*
 buffer_getc(Buffer const* self);
 
 /**
- * Desc.
+ * Clear buffer.
  *
- * @param[in] 
- * @return	  
+ * @param[in] self 
  */
 void
 buffer_clear(Buffer* self);
 
 /**
- * Desc.
+ * Push data to buffer.
  *
- * @param[in] 
- * @return	  
+ * @param[in] self 
+ * @param[in] ch   Push data.
+ * @return         Success to length of buffer.
+ * @return         Failed to zero.
  */
 size_t
 buffer_push(Buffer* self, int ch);
 
 /**
+ * Resize buffer.
  *
+ * @param[in] self		
+ * @param[in] resize	Number of resize size.	
+ * @return				Success to true.
+ * @return				Failed to false.
  */
 bool
 buffer_resize(Buffer* self, size_t resize);
 
 /**
- * Desc.
+ * Check empty is buffer.
  *
  * @param[in] 
- * @return	  
+ * @return		Empty to true.
+ * @return		Not empty to false.
  */
 bool
 buffer_empty(Buffer const* self);
 
 /**
- * Desc.
+ * Read line from stream, And write to buffer.
  *
- * @param[in]
+ * @param[in] stream	Source stream.
+ * @return				Success to true.
+ * @return				Done or failed to false.
  */
 bool
 buffer_getline(Buffer* self, FILE* stream);
 
 /**
+ * Copy string to buffer.
  *
+ * @param[in] self	
+ * @param[in] src	Copy source string.
+ * @return			Success to true.
+ * @return			Failed to false.
  */
 bool
 buffer_copy_str(Buffer* self, char const* src);
