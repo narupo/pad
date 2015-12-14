f9b07b32a51cd678bfbf223a3ba9f7b171d82c7b
narupo <narupo.glab@gmail.com>
2015-12-14 18:05:15 +0900

    cat: implements 's' option

--- src/cap-file.h
+++ src/cap-file.h
@@ -19,6 +19,7 @@ typedef enum {
 	CapColMark,
 	CapColRun,
 	CapColGoto,
+	CapColSeparate,
 } CapColType;
 
 
--- src/config.h
+++ src/config.h
@@ -15,9 +15,20 @@
 * Delete and New *
 *****************/
 
+/**
+ * Destruct config
+ *
+ * @param[in] self pointer to config object
+ */
 void
 config_delete(Config* self);
 
+/**
+ * Construct config
+ *
+ * @return success to pointer to config object of dynamic allocate memory
+ * @return failed to pointer to NULL
+ */
 Config*
 config_new(void);
 
@@ -25,12 +36,41 @@ config_new(void);
 * Getter *
 *********/
 
+/**
+ * Make normalized cap's path from basename
+ *
+ * @param[in] self 
+ * @param[in] dst destination of normalized path
+ * @param[in] dstsize size of destination
+ * @param[in] basename basename
+ *
+ * @return success to pointer to dst
+ * @return failed to pointer to NULL
+ */
 char*
 config_path_from_base(Config const* self, char* dst, size_t dstsize, char const* basename);
 
+/**
+ * Make normalized cap's path from basename
+ * 
+ * @param[in] self
+ * @param[in] basename basename
+ *
+ * @return success to pointer to normalized path of dynamic allocate memory
+ * @return failed to pointer to NULL
+ */
 char*
 config_make_path_from_base(Config const* self, char const* basename);
 
+/**
+ * Get path by key name
+ *
+ * @param[in] self
+ * @param[in] key key name (example "cd" or "editor")
+ *
+ * @return success to pointer to path
+ * @return failed to NULL
+ */
 char const*
 config_path(Config const* self, char const* key);
 
@@ -38,9 +78,27 @@ config_path(Config const* self, char const* key);
 * Setter *
 *********/
 
+/**
+ * Set path by key name
+ *
+ * @param[in] self
+ * @param[in] key key name
+ * @param[in] path set path
+ *
+ * @return success to true
+ * @return failed to false
+ */
 bool
 config_set_path(Config* self, char const* key, char const* path);
 
+/**
+ * Save config to file system
+ *
+ * @param self
+ *
+ * @return success to true
+ * @return failed to false
+ */
 bool
 config_save(Config const* self);
 
