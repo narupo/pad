112a24717b37b626a52ba5f3332e19ed66ef8ed0
narupo <narupo.glab@gmail.com>
2015-12-25 10:31:49 +0900

    caperr, alias key padding

--- src/caperr.h
+++ src/caperr.h
@@ -1,16 +1,21 @@
 #ifndef CAPERR_H
 #define CAPERR_H
 
+#include "term.h"
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include <ctype.h>
 #include <errno.h>
+#include <pthread.h>
+#include <stdbool.h>
 
 enum {
 	CAPERR_CONSTRUCT = 1,  // Need 1 origin
 	CAPERR_FOPEN,
 	CAPERR_PARSE_OPTIONS,
 	CAPERR_INVALID_ARGUMENTS,
+	CAPERR_MUTEX_LOCK,
 };
 
 int
