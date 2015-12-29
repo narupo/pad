6ace82db8d4335cf06500fc94b44abeb49abd294
narupo <narusawayu@gmail.com>
2015-12-30 07:34:05 +0900

    brief

---
+++ src/brief.h
@@ -0,0 +1,35 @@
+#ifndef BRIEF_H
+#define BRIEF_H
+
+#include "term.h"
+#include "util.h"
+#include "config.h"
+#include "file.h"
+#include "cap-file.h"
+#include "atcap.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <string.h>
+
+/**
+ * @brief 
+ *
+ * @param void 
+*/
+void 
+brief_usage(void);
+
+/**
+ * @brief 
+ *
+ * @param argc   
+ * @param argv[] 
+ *
+ * @return 
+*/
+int 
+brief_main(int argc, char* argv[]);
+
+#endif
