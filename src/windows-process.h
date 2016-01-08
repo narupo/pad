939e316d512d6e2e3cb1845ef8a653a45d727c75
narupo <narusawayu@gmail.com>
2016-01-09 00:41:48 +0900

    process

---
+++ src/windows-process.h
@@ -0,0 +1,34 @@
+#ifndef WINDOWSPROCESS_H
+#define WINDOWSPROCESS_H
+
+#include <windows.h>
+
+typedef struct {
+	int debug;
+} WindowsProcess;
+
+void
+winprocess_delete(WindowsProcess* self) {
+	if (self) {
+		free(self);
+	}
+}
+
+WindowsProcess*
+winprocess_new(void) {
+	WindowsProcess* self = (WindowsProcess*) calloc(1, sizeof(WindowsProcess));
+	if (!self) {
+		perror("WindowsProcess");
+		return NULL;
+	}
+
+	return self;
+}
+
+bool
+winprocess_start(WindowsProcess* self) {
+	return false;
+}
+
+#endif
+
