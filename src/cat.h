687a6869ecad2c103c75af80e89bee509b709546
narupo <narupo.glab@gmail.com>
2016-01-14 23:08:22 +0900

    add comments

--- src/cat.h
+++ src/cat.h
@@ -16,18 +16,37 @@
 #include <getopt.h>
 #include <ctype.h>
 
+/**
+ * @brief      Display usage of command
+ */
 void
 cat_usage(void);
 
+/**
+ * @brief      Entry function of cat command
+ *
+ * @param[in]  argc  like a main function's argument
+ * @param[in]  argv  like a main function's argument
+ *
+ * @return     success to a number of zero
+ * @return     failed to a number of caperr
+ */
 int
 cat_main(int argc, char* argv[]);
 
 /**
  * This Interface for the cap make command
  * Do not clear and delete CapFile because append row to it for make
+ *
+ * @param      config   pointer to Config
+ * @param      dstfile  pointer to destination CapFile
+ * @param[in]  argc     number of elements of argv
+ * @param      argv     pointer to array of pointer
+ *
+ * @return     success to a number of zero
+ * @return     failed to a number of caperr
  */
 int
 cat_make(Config const* config, CapFile* dst, int argc, char* argv[]);
 
 #endif
-
