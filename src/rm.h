1c80d17398b379c0586be23ac68fca6ae7a0964a
narupo <narupo.glab@gmail.com>
2016-01-29 17:40:47 +0900

    implementation rm command

---
+++ src/rm.h
@@ -0,0 +1,15 @@
+#ifndef RM_H
+#define RM_H
+
+#include "util.h"
+#include "term.h"
+#include "caperr.h"
+#include "config.h"
+
+void
+rm_usage(void);
+
+int
+rm_main(int argc, char* argv[]);
+
+#endif
