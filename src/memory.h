1b713cf310d68a9681ca5a90fa1013799b13f605
narupo <narusawayu@gmail.com>
2016-01-31 05:00:33 +0900

    str

---
+++ src/memory.h
@@ -0,0 +1,28 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include "util.h"
+#include <stdlib.h>
+
+void
+mem_free(void* ptr);
+
+void*
+mem_malloc(size_t size);
+
+void*
+mem_calloc(size_t nmemb, size_t size);
+
+void*
+mem_realloc(void* ptr, size_t size);
+
+void*
+mem_emalloc(size_t size);
+
+void*
+mem_ecalloc(size_t nmemb, size_t size);
+
+void*
+mem_erealloc(void* ptr, size_t size);
+
+#endif
