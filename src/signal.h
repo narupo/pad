a9ff7798c8c5888902ba8e158a6c399aedde7f6f
narupo <narusawayu@gmail.com>
2016-02-08 19:34:33 +0900

    signal

--- src/signal.h
+++ src/signal.h
@@ -1,6 +1,18 @@
 #ifndef SIGNAL_H
 #define SIGNAL_H
 
+#include <stdio.h>
+#include <signal.h>
 
+#if defined(_WIN32) || defined(_WIN64)
+// Nothing todo
+#else
+
+typedef void (*sighandler_t)(int);
+
+sighandler_t
+signal(int sig, sighandler_t handler);
 
 #endif
+
+#endif /* SIGNAL_H */
