0eef9d31dd2ff89797a58ccbaca8ef0d2c62d651
narupo <narusawayu@gmail.com>
2016-03-04 15:37:50 +0900

    Logo for CapServer

--- src/term.h
+++ src/term.h
@@ -6,15 +6,28 @@
 
 // Macros for color string on terminal
 // Example: printf(TERM_RED "This is red string." TERM_RESET " Reseted.");
-#define TERM_NRM     "\x1B[0m"
-#define TERM_RED     "\x1B[31m"
-#define TERM_GREEN   "\x1B[32m"
-#define TERM_YELLOW  "\x1B[33m"
-#define TERM_BLUE    "\x1B[34m"
-#define TERM_MAGENTA "\x1B[35m"
-#define TERM_CYAN    "\x1B[36m"
-#define TERM_WHITE   "\x1B[37m"
-#define TERM_RESET   "\033[0m"
+// #if defined(_WIN32) || defined(_WIN64)
+// #  define TERM_NRM     
+// #  define TERM_RED     
+// #  define TERM_GREEN   
+// #  define TERM_YELLOW  
+// #  define TERM_BLUE    
+// #  define TERM_MAGENTA 
+// #  define TERM_CYAN    
+// #  define TERM_WHITE   
+// #  define TERM_RESET  
+#if 0
+#else
+#  define TERM_NRM     "\x1B[0m"
+#  define TERM_RED     "\x1B[31m"
+#  define TERM_GREEN   "\x1B[32m"
+#  define TERM_YELLOW  "\x1B[33m"
+#  define TERM_BLUE    "\x1B[34m"
+#  define TERM_MAGENTA "\x1B[35m"
+#  define TERM_CYAN    "\x1B[36m"
+#  define TERM_WHITE   "\x1B[37m"
+#  define TERM_RESET   "\033[0m"
+#endif
 
 /**
  * Wrapper of fflush(stdout)
