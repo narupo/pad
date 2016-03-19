b21ef5c597df750a2c94c75be5cc2e75ec8e6d6c
narupo <narupo.glab@gmail.com>
2016-03-19 21:32:25 +0900

    Add comments. Add hash.h

---
+++ src/hash.h
@@ -0,0 +1,46 @@
+#ifndef HASH_H
+#define HASH_H
+
+enum {
+	HASH_NHASH = 701,
+};
+
+/**
+ * Create hash value from string
+ * 
+ * @param[in] src source string
+ * @param[in] nhash max hash size
+ *
+ * @return number of hash value
+ */
+static inline int
+hash_int(char const* src) {
+	int n = 0;
+
+	for (char const* p = src; *p; ++p) {
+		n += *p;
+	}
+
+	return n % HASH_NHASH;
+}
+
+/**
+ * Create hash value from string and number
+ * 
+ * @param[in] src source string
+ * @param[in] nhash max hash size
+ *
+ * @return number of hash value
+ */
+static inline int
+hash_int_from(char const* src, int nhash) {
+	int n = 0;
+
+	for (char const* p = src; *p; ++p) {
+		n += *p;
+	}
+	
+	return n % nhash;
+}
+
+#endif
