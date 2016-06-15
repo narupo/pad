004be741cfeaca530e3f10653110167513c98074
narupo <narusawayu@gmail.com>
2016-06-16 01:44:13 +0900

    Testing.

--- src/cap.c
+++ src/cap.c
@@ -1,8 +1,28 @@
 #include "cap.h"
 
 int
-main(int argc, char *argv[]) {
-	printf("Hello, World!\n");
+main(int argc, char *ap[]) {
+	const char *pname = ap[1];
+	char *const *argv = (char *const *)ap+1;
+
+	char ppath[100];
+	snprintf(ppath, sizeof ppath, "%s/%s", getenv("CAP_BINDIR"), pname);
+
+	pid_t pid = fork();
+	if (pid == -1) {
+		cap_die("fork");
+	}
+
+	if (pid == 0) {
+		// Child
+		if (execv(ppath, argv) == -1) {
+			cap_die("execv");
+		}
+	} else {
+		// Parent
+		wait(NULL);
+		exit(0);
+	}
+
 	return 0;
 }
-
--- src/cap.h
+++ src/cap.h
@@ -1,6 +1,11 @@
 #ifndef CAP_H
 #define CAP_H
 
+#define _BSD_SOURCE 1 /**/
 #include <stdio.h>
+#include <sys/wait.h>
+
+#include "error.h"
+#include "file.h"
 
 #endif
