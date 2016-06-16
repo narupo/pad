fa40b408c80347334e1f462faabcb407a3ba2bb8
narupo <narusawayu@gmail.com>
2016-06-16 18:03:36 +0900

    Testing.

--- src/cap-cd.c
+++ src/cap-cd.c
@@ -1,43 +1,47 @@
 #include "cap-cd.h"
 
-int
-main(int argc, char* argv[]) {
-	if (argc < 2) {
-		const char *cd = getenv("CAP_CD");
-		if (!cd) {
-			cap_log("error", "need environ variable of cd");
-			return 1;
-		}
-		printf("%s\n", cd);
-		return 0;
-	}
-	
-	char newcd[100];
-	cap_fsolve(newcd, sizeof newcd, argv[1]);
-	
-	if (!cap_fisdir(newcd)) {
-		cap_log("error", "can't move to %s", newcd);
-		return 1;
-	}
-
+static bool
+docd(const char *newcd) {
 	const char *vardir = getenv("CAP_VARDIR");
 	if (!vardir) {
-		cap_log("error", "need environ variable of vardir");
-		return 1;
+		cap_die("need environment variable of vardir");
 	}
-	
+
+	if (!cap_fisdir(newcd)) {
+		return false;
+	}
+
 	char cdpath[100];
 	snprintf(cdpath, sizeof cdpath, "%s/cd", vardir);
 	// printf("vardir[%s]\n", cdpath);
 
 	FILE *fout = fopen(cdpath, "w");
 	if (!fout) {
-		cap_log("error", "fopen %s", cdpath);
-		return 1;
+		return false;
 	}
 
 	fprintf(fout, "%s", newcd);
 
-	fclose(fout);
+	return fclose(fout) == 0;
+}
+
+int
+main(int argc, char* argv[]) {
+	if (argc < 2) {
+		const char *home = getenv("CAP_HOME");
+		home = (home ? home : "/tmp");
+		if (!docd(home)) {
+			cap_die("failed to cd");
+		}
+		return 0;
+	}
+	
+	char newcd[100];
+	cap_fsolve(newcd, sizeof newcd, argv[1]);
+	
+	if (!docd(newcd)) {
+		cap_die("failed to cd");
+	}
+
 	return 0;
 }
