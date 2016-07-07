5962d7288543c8c310833f90db1a0269061967a7
narupo <narusawayu@gmail.com>
2016-07-08 03:40:53 +0900

    cap-ls

---
+++ src/cap-ls.h
@@ -0,0 +1,12 @@
+#ifndef CAP_LS_H
+#define CAP_LS_H
+
+#undef _GNU_SOURCE
+#define _GNU_SOURCE 1 /* In cap-ls for strdup */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "error.h"
+#include "file.h"
+
+#endif
