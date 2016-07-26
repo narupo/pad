ee379ee2ffcc57e36c797b2623a1e44bc6ea3f04
narupo <narusawayu@gmail.com>
2016-07-26 11:06:17 +0900

    Refactoring.

--- src/cap-cat.h
+++ src/cap-cat.h
@@ -1,11 +1,12 @@
 #ifndef CAP_CAT_H
 #define CAP_CAT_H
 
+#define _GNU_SOURCE 1 /* in cap-alias.h for setenv */
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "error.h"
 #include "file.h"
 #include "config.h"
-
-#include <stdio.h>
-#include <stdlib.h>
 
 #endif
