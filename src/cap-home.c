375d3cdb54519d7070adfd6819c4ff21da7d627a
narupo <narupo.glab@gmail.com>
2016-07-31 19:16:42 +0900

    Refactoring.

--- src/cap-home.c
+++ src/cap-home.c
@@ -3,23 +3,24 @@
 int
 main(int argc, char *argv[]) {
 	if (argc < 2) {
-		const char *home = getenv("CAP_VARHOME");
-		if (!home) {
+		char home[FILE_NPATH];
+		if (!cap_envget(home, sizeof home, "CAP_VARHOME")) {
 			cap_die("need environment variable of home");
 		}
 		printf("%s\n", home);
 		return 0;
 	}
 
-	const char *vardir = getenv("CAP_VARDIR");
-	if (!vardir || strlen(vardir) == 0) {
+	char vardir[FILE_NPATH];
+	if (!cap_envget(vardir, sizeof vardir, "CAP_VARDIR") ||
+		strlen(vardir) == 0) {
 		cap_die("need environment variable of vardir");
 	}
 
-	char hmpath[100];
+	char hmpath[FILE_NPATH];
 	snprintf(hmpath, sizeof hmpath, "%s/home", vardir);
 
-	char newhome[100];
+	char newhome[FILE_NPATH];
 	cap_fsolve(newhome, sizeof newhome, argv[1]);
 	if (!cap_fisdir(newhome)) {
 		cap_die("%s is not a directory", newhome);
@@ -29,7 +30,7 @@ main(int argc, char *argv[]) {
 	cap_fwriteline(newhome, hmpath);
 
 	// Update var/cd
-	char cdpath[100];
+	char cdpath[FILE_NPATH];
 	snprintf(cdpath, sizeof cdpath, "%s/cd", vardir);
 	cap_fwriteline(newhome, cdpath);
 
