59850933587b75a8500d994398af33d7c6093625
narupo <narusawayu@gmail.com>
2016-08-01 15:09:58 +0900

    args

--- src/args.c
+++ src/args.c
@@ -1,12 +1,19 @@
 #include "args.h"
 
+/*********
+* number *
+*********/
+
 enum {
 	NCAPA = 4,
 };
 
-// arg
+/******
+* arg *
+******/
 
 struct cap_arg {
+	cap_argtype_t type;
 	char *value;
 };
 
@@ -19,17 +26,33 @@ argdel(struct cap_arg *self) {
 }
 
 struct cap_arg *
-argnew(void) {
+cap_argnew(void) {
 	struct cap_arg *self = calloc(1, sizeof(*self));
 	return self;
 }
 
 struct cap_arg *
-argparse(struct cap_arg *self, const char *src) {
+cap_argparse(struct cap_arg *self, const char *src) {
+	if (self->value) {
+		free(self->value);
+	}
+
+	self->value = strdup(src);
+	if (!self->value) {
+		return NULL;
+	}
+
 	return self;
 }
 
-// args
+void
+cap_argshow(const struct cap_arg *self, FILE *fout) {
+	fprintf(fout, "<cap_arg value=\"%s\">\n", self->value);
+}
+
+/*******
+* args *
+*******/
 
 struct cap_args {
 	struct cap_arg **args;
@@ -68,27 +91,56 @@ cap_argsclear(struct cap_args *self) {
 }
 
 struct cap_args *
-cap_argsmoveb(struct cap_args *self, struct cap_arg *arg) {
+cap_argsresize(struct cap_args *self, int newcapa) {
+	struct cap_arg **tmp = realloc(self->args, sizeof(struct cap_arg *) * newcapa + 1);
+	if (!tmp) {
+		return NULL;
+	}
 
+	self->capa = newcapa;
+	self->args = tmp;
+
+	return self;
 }
 
 struct cap_args *
-cap_argsparse(struct cap_args *self, int argc, const char *argv[]) {
+cap_argsmove(struct cap_args *self, struct cap_arg *arg) {
+	if (self->len >= self->capa) {
+		if (!cap_argsresize(self, self->capa*2)) {
+			perror("resize args");
+		}
+	}
+
+	self->args[self->len++] = arg;
+
+	return self;
+}
+
+struct cap_args *
+cap_argsparse(struct cap_args *self, int argc, char *argv[]) {
 	cap_argsclear(self);
 	for (int i = 0; i < argc; ++i) {
-		struct cap_arg *arg = argnew();
-		argparse(arg, argv[i]);
-		cap_argsmoveb(self, arg);
+		struct cap_arg *arg = cap_argnew();
+		cap_argparse(arg, argv[i]);
+		cap_argsmove(self, arg);
 	}
 	return self;
+}
+
+void
+cap_argsshow(const struct cap_args *self, FILE *fout) {
+	for (int i = 0; i < self->len; ++i) {
+		cap_argshow(self->args[i], fout);
+	}
 }
 
 #if defined(_TEST_ARGS)
 int
 main(int argc, char *argv[]) {
-	struct cap_args *args = cap_argsnew()
+	struct cap_args *args = cap_argsnew();
 
 	cap_argsparse(args, argc, argv);
+	cap_argsshow(args, stderr);
 
 	cap_argsdel(args);
 	return 0;
--- src/args.h
+++ src/args.h
@@ -1,7 +1,13 @@
 #ifndef ARGS_H
 #define ARGS_H
 
+#define _GNU_SOURCE 1 /* args.h: strdup */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+	ARG_UNKNOWN,
+} cap_argtype_t;
 
 #endif
