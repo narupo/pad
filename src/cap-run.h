b5103232a2e5ff1e2bfe2c15e0bb5589e4d959f0
narupo <narusawayu@gmail.com>
2016-08-19 11:16:25 +0900

    Fixed: typo.

--- src/cap-run.h
+++ src/cap-run.h
@@ -8,7 +8,7 @@
 #ifndef CAP_RUN_H
 #define CAP_RUN_H
 
-#define _BSD_SOURCE 1 /* cap-run.h: popen */
+#define _BSD_SOURCE 1 /* cap: cap-run.h: popen */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
