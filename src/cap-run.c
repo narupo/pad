98180528d580a12c6be2dbe5b0e97ab01bf9af4f
narupo <narupo.glab@gmail.com>
2017-03-17 14:17:58 +0900

    Refactoring.

--- src/cap-run.c
+++ src/cap-run.c
@@ -39,6 +39,8 @@ readscriptline(char *dst, size_t dstsz, const char *path) {
 }
 
 struct opts {
+    int argc;
+    char **argv;
     bool ishelp;
 };
 
@@ -47,7 +49,6 @@ optsparse(struct opts *self, int argc, char *argv[]) {
     // Parse options
     static struct option longopts[] = {
         {"help", no_argument, 0, 'h'},
-        {"fname", required_argument, 0, 'f'},
         {},
     };
 
@@ -57,7 +58,7 @@ optsparse(struct opts *self, int argc, char *argv[]) {
 
     for (;;) {
         int optsindex;
-        int cur = getopt_long(argc, argv, "hf:", longopts, &optsindex);
+        int cur = getopt_long(argc, argv, "h", longopts, &optsindex);
         if (cur == -1) {
             break;
         }
@@ -75,94 +76,90 @@ optsparse(struct opts *self, int argc, char *argv[]) {
         return false;
     }
 
+    self->argc = argc;
+    self->argv = argv;
+
     return true;
 }
 
-static struct cap_array *
-fixargs(int argc, char *argv[]) {
-    const char *optslist[] = {
-        "-h",
-        "--help",
-        "-g",
-        "--global",
-        NULL,
-    };
-    int m = 0;
+struct app {
+    struct opts opts;
+};
 
-    struct cap_array *args = cap_arrnew();
-    if (!args) {
+static void
+appdel(struct app *self) {
+    if (self) {
+        free(self);
+    }
+}
+
+static struct app *
+appnew(int argc, char *argv[]) {
+    struct app *self = calloc(1, sizeof(*self));
+    if (!self) {
         return NULL;
     }
 
-    for (int i = 0; i < argc; ++i) {
-        const char *arg = argv[i];
-        switch (m) {
-        case 0:
-            if (strcmp(arg, "run") == 0) {
-                cap_arrpush(args, arg);
-                m = 1;
-            }
-        break;
-        case 1: {
-            bool matched = false;
-            for (int i = 0; optslist[i]; ++i) {
-                if (strcmp(optslist[i], arg) == 0) {
-                    matched = true;
-                    break; // through
-                }
-            }
-            if (!matched) {
-                cap_arrpush(args, arg);
-                m = 2;                
-            }
-        } break;
-        case 2:
-            cap_arrpush(args, arg);
-        break;
-        }
+    if (!optsparse(&self->opts, argc, argv)) {
+        free(self);
+        return NULL;
     }
 
-    return args;
+    return self;
+}
+
+static int32_t
+appmain(struct app *self) {
+    if (self->opts.argc < 2) {
+        cap_error("need script name");
+        return 1;
+    }
+
+    char varcd[FILE_NPATH];
+    if (!cap_envget(varcd, sizeof varcd, "CAP_VARCD")) {
+        cap_error("need environment variable of cd");
+        return 1;
+    }
+
+    char spath[FILE_NPATH]; // Script path
+    cap_fsolvefmt(spath, sizeof spath, "%s/%s", varcd, self->opts.argv[1]);
+    if (isoutofhome(spath)) {
+        cap_die("invalid script. '%s' is out of home.", spath);
+    }
+
+    char exesname[NSCRIPTNAME]; // Execute script name in file
+    readscriptline(exesname, sizeof exesname, spath);
+
+    struct cap_string *cmdline = cap_strnew();
+    cap_strapp(cmdline, exesname);
+    cap_strapp(cmdline, " ");
+    cap_strapp(cmdline, spath);
+    cap_strapp(cmdline, " ");
+    for (int i = 2; i < self->opts.argc; ++i) {
+        cap_strapp(cmdline, self->opts.argv[i]);
+        cap_strapp(cmdline, " ");
+    }
+
+    // Start process communication
+    safesystem(cap_strgetc(cmdline));
+    
+    // Done
+    cap_strdel(cmdline);
+    return 0;
 }
 
 int
 main(int argc, char *argv[]) {
 	cap_envsetf("CAP_PROCNAME", "cap run");
 
-	if (argc < 2) {
-		cap_error("need script name");
-		return 1;
-	}
+    struct app *app = appnew(argc, argv);
+    if (!app) {
+        cap_error("failed to create application");
+        return 1;
+    }
 
-	char varcd[FILE_NPATH];
-	if (!cap_envget(varcd, sizeof varcd, "CAP_VARCD")) {
-		cap_error("need environment variable of cd");
-		return 1;
-	}
+    int32_t ret = appmain(app);
+    appdel(app);
 
-	char spath[FILE_NPATH]; // Script path
-	cap_fsolvefmt(spath, sizeof spath, "%s/%s", varcd, argv[1]);
-	if (isoutofhome(spath)) {
-		cap_die("invalid script. '%s' is out of home.", spath);
-	}
-
-	char exesname[NSCRIPTNAME]; // Execute script name in file
-	readscriptline(exesname, sizeof exesname, spath);
-
-	struct cap_string *cmdline = cap_strnew();
-	cap_strapp(cmdline, exesname);
-	cap_strapp(cmdline, " ");
-	cap_strapp(cmdline, spath);
-	cap_strapp(cmdline, " ");
-	for (int i = 2; i < argc; ++i) {
-		cap_strapp(cmdline, argv[i]);
-		cap_strapp(cmdline, " ");
-	}
-
-	// Start process communication
-	safesystem(cap_strgetc(cmdline));
-	
-	// Done
-	cap_strdel(cmdline);
-	return 0;
+    return ret;
 }
