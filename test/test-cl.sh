9eb78f7c3d5c1ca0dccaaba15226eb7ebc7ce791
narupo <narupo.glab@gmail.com>
2017-04-17 06:20:45 +0900

    Remove files.

--- test/test-cl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-valgrind ./test-cl parsestr "123 \"223 323\" &> abebe '|' '123 \'223'  123\"223\"323 \"223\n323\t\"  '423\n523\t' -a -abc value -a=ABC -b=\"ABC DEF\" --A --ABC VALUE | --eq=EQ --EQ=\"EQ \\n \\\"EQ\" > /tmp/null"
-if [ "$?" -ne 0 ]; then
-	echo "failed."
-	exit 1
-fi
