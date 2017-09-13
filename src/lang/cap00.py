93288f484be27e90554011d7baeb1f4f68a9e498
narupo <narupo.glab@gmail.com>
2017-09-13 18:49:52 +0900

    try-and-error

--- src/lang/cap00.py
+++ src/lang/cap00.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import sys
-from tokenizer import SpaceTokenizer
+from tokenizer import CapTokenizer
 
 class NilNode:
 
@@ -13,11 +13,46 @@ class ValueNode:
     def __init__(self):
         self.value = None
 
+    def __str__(self):
+        return str(self.value)
+
 class BinNode:
 
     def __init__(self):
         self.lhs = None
         self.rhs = None
+
+class OpNode:
+
+    def __init__(self):
+        self.operand = None
+        self.lhs = None
+        self.rhs = None
+
+    def __str__(self):
+        return str(self.operand)
+
+    def __calc(self, n, dep=0, side='?'):
+        print('_' * dep, side, str(n))
+        if type(n) == OpNode:
+            v = self.__calc(n.lhs, dep+1, 'L')
+            if n.operand == '+':
+                v += self.__calc(n.rhs, dep+1, 'R')
+            elif n.operand == '-':
+                v -= self.__calc(n.rhs, dep+1, 'R')
+            elif n.operand == '*':
+                v *= self.__calc(n.rhs, dep+1, 'R')
+            elif n.operand == '/':
+                v /= self.__calc(n.rhs, dep+1, 'R')
+            return v
+        elif type(n) == ValueNode:
+            return int(n.value)
+        elif type(n) == VariableNode:
+            raise Exception('TODO')
+        raise Exception('unsupported node')
+
+    def calc(self):
+        return self.__calc(self)
 
 class IfNode:
 
@@ -27,12 +62,38 @@ class IfNode:
         self.nelif = None
         self.nelse = None
 
+class ForNode:
+    """
+        for
+       /   \       \      \
+      init compare update then
+    """
+
+    def __init__(self):
+        self.ncompare = None
+        self.nthen = None
+        self.ninit = None
+        self.nupdate = None
+
+class VariableNode:
+
+    def __init__(self):
+        self.identifier = None
+
 """
 program ::= [ plain | block ]*
 plain ::= !('{{'|'}}')
 block ::= '{{' [ code ]* '}}'
-code ::= statement
-statement ::= if-statement | print-statement
+code ::= statement | expr
+
+expr ::= term, [ ('+'|'-') term ]*
+term ::= factor, [ ('*'|'/') factor ]*
+factor ::= mono | '(' expr ')'
+mono ::= number | variable
+number ::= [ 0-9]*
+variable ::= [ a-z | A-Z | _ ]* [ 0-9 ]*
+
+statement ::= for-statement | if-statement | print-statement
 
 print-statement ::= 'print' '(' print-content ')'
 print-content ::= [ a-z|A-Z|0-9 ]*
@@ -42,16 +103,23 @@ if-compare ::= 1 | 0
 if-else ::= 'else' '{' code '}'
 if-elif ::= if-statement
 
+for-statement ::= for-statement-1 | for-statement-2
+for-statement-1 ::= 'for' '{' code '}'
+for-statement-2 ::= 'for' for-compare '{' code '}'
+for-statement-3 ::= 'for' for-init ';' for-compare ';' for-update '{' code '}'
+
 """
 class App:
 
     def __init__(self):
-        self.tkr = SpaceTokenizer()
+        self.tkr = CapTokenizer()
         self.root = None
         self.isdebug = False
+        self.calc = None # 演算結果が入るレジスタ
 
     def run(self):
         self.tkr.parse(sys.stdin)
+        for t in self.tkr.toks: print(t)
         self.root = self.program()
         self.traverse(self.root, side='R')
 
@@ -72,6 +140,12 @@ class App:
                 self.traverse(node.nelif, dep+1, side='elif')
             elif node.nelse:
                 self.traverse(node.nelse, dep+1, side='else')
+        elif type(node) == ForNode:
+            while node.ncompare.value:
+                self.traverse(node.nthen, dep+1, side='then')
+            raise Exception('TODO')
+        elif type(node) == OpNode:
+            print(node.calc())
 
     def program(self):
         root = BinNode()
@@ -108,17 +182,157 @@ class App:
         return root
 
     def code(self):
-        return self.statement()
+        n = self.statement()
+        if not n:
+            n = self.expr()
+        return n
+
+    """
+expr ::= term, op1 expr
+op1 ::= ('+'|'-')
+term ::= factor, op2 expr
+op2 ::= ('*'|'/')
+factor ::= mono | '(' expr ')'
+mono ::= number | variable
+number ::= [ 0-9]*
+variable ::= [ a-z | A-Z | _ ]* [ 0-9 ]*
+    """
+    def expr(self):
+        root = None
+
+        n1 = self.term()
+        if self.tkr.cur() in ['+', '-']:
+            root = OpNode()
+            root.lhs = n1
+            root.operand = self.tkr.get()
+            root.rhs = self.expr()
+        else:
+            root = n1
+
+        return root
+
+    def term(self):
+        root = None
+
+        n1 = self.factor()
+        if self.tkr.cur() in ['*', '/']:
+            root = OpNode()
+            root.lhs = n1
+            root.operand = self.tkr.get()
+            root.rhs = self.expr()
+        else:
+            root = n1
+
+        return root
+
+    def factor(self):
+        root = None
+
+        if self.tkr.cur() == '(':
+            self.tkr.get() # '('
+            root = self.expr()
+            self.tkr.get() # ')'
+        else:
+            root = self.mono()
+
+        return root
+
+    def mono(self):
+        root = None
+
+        n1 = self.tkr.get()
+        if n1.isdigit():
+            root = ValueNode()
+            root.value = n1
+        else:
+            root = VariableNode()
+            root.identifier = n1
+
+        return root
 
     def statement(self):
         if self.tkr.cur() == 'if':
             return self.if_statement()
         elif self.tkr.cur() == 'print':
             return self.print_statement()
+        elif self.tkr.cur() == 'for':
+            return self.for_statement()
         elif self.tkr.cur() == ';':
             self.tkr.get() # ;
             return NilNode()
-        raise SyntaxError(self.tkr.cur())
+        return None
+
+    def for_statement(self):
+        if self.tkr.cur(1) == '{':
+            return self.for_statement_1()
+        return self.for_statement_2()
+
+    def for_init(self):
+        while self.tkr.cur() not in [';', '{']:
+            self.tkr.get()
+        return ValueNode() # TODO
+
+    def for_compare(self):
+        ncompare = ValueNode()
+        ncompare.value = 1
+        while self.tkr.cur() not in [';', '{']:
+            self.tkr.get()
+        return ncompare
+
+    def for_update(self):
+        while self.tkr.cur() not in ['{']:
+            self.tkr.get()
+        return ValueNode() # TODO
+
+    def for_statement_1(self):
+        """
+            for { code }
+        """
+        nfor = ForNode()
+
+        self.tkr.get() # for
+        nfor.ncompare = ValueNode()
+        nfor.ncompare.value = 1 # eternal loop
+        self.tkr.get() # {
+
+        nfor.nthen = self.code()
+        self.tkr.get() # }
+
+        return nfor
+
+    def for_statement_2(self):
+        """
+            for init; compare; update { code }
+        """
+        nfor = ForNode()
+
+        self.tkr.get() # for
+        nfor.ninit = self.for_init()
+        self.tkr.get() # ;
+        nfor.ncompare = self.for_compare()
+        self.tkr.get() # ;
+        nfor.nupdate = self.for_update()
+        self.tkr.get() # {
+
+        nfor.nthen = self.code()
+        self.tkr.get() # }
+
+        return nfor
+
+    def for_statement_3(self):
+        """
+            for compare { code }
+        """
+        nfor = ForNode()
+
+        self.tkr.get() # for
+        nfor.ncompare = self.for_compare()
+        self.tkr.get() # {
+
+        nfor.nthen = self.code()
+        self.tkr.get() # }
+
+        return nfor
 
     def print_statement(self):
         nprint = ValueNode()
@@ -157,7 +371,9 @@ class App:
 
 
 def main():
-    App().run()
+    app = App()
+    app.isdebug = True
+    app.run()
     sys.exit(0)
 
 if __name__ == '__main__':
--- src/lang/tokenizer.py
+++ src/lang/tokenizer.py
@@ -1,8 +1,52 @@
+class Stream:
+    EOF = (-1)
+
+    def __init__(self, src):
+        self._src = src
+        self._limit = len(src)
+        self._index = 0
+
+    def eof(self):
+        return self._index >= self._limit or self._index < 0
+
+    def get(self):
+        if self.eof():
+            return self.EOF
+
+        ch = self._src[self._index]
+        self.next()
+        return ch
+
+    def cur(self, ofs=0):
+        i = self._index + ofs
+        if i < self._limit and i >= 0:
+            return self._src[i]
+        return self.EOF
+
+    def prev(self):
+        if self._index > 0:
+            self._index -= 1
+
+    def next(self):
+        if self._index < self._limit:
+            self._index += 1
+
 class Tokenizer():
 
-    def parse(self, fin): raise Exception()
-    def cur(self): raise Exception()
-    def get(self): raise Exception()
+    def parse(self, fin):
+        raise Exception()
+
+    def cur(self, ofs=0):
+        if self.pos+ofs >= len(self.toks) or self.pos+ofs < 0:
+            return None
+        return self.toks[self.pos+ofs]
+    
+    def get(self):
+        if self.pos >= len(self.toks):
+            return None
+        t = self.toks[self.pos]
+        self.pos += 1
+        return t
 
 class SpaceTokenizer(Tokenizer):
 
@@ -15,14 +59,51 @@ class SpaceTokenizer(Tokenizer):
         s = s.replace('(', ' ( ').replace(')', ' ) ')
         self.toks = s.split()
 
-    def cur(self):
-        if self.pos >= len(self.toks) or self.pos < 0:
-            return None
-        return self.toks[self.pos]
-    
-    def get(self):
-        if self.pos >= len(self.toks):
-            return None
-        t = self.toks[self.pos]
-        self.pos += 1
-        return t
+class CapTokenizer(Tokenizer):
+
+    def __init__(self):
+        self.stream = None
+        self.toks = []
+        self.pos = 0
+        self.buf = ''
+
+    def push(self):
+        if len(self.buf):
+            self.toks.append(str(self.buf))
+            self.buf = ''
+
+    def parse(self, fin):
+        self.stream = Stream(fin.read())
+        self.toks = []
+        self.pos = 0
+        self.buf = ''
+        m = 0
+
+        while not self.stream.eof():
+            c = self.stream.get()
+
+            if m == 0: # first
+                if c.isspace():
+                    self.push()
+                elif c == '+':
+                    self.push()
+                    m = 100
+                elif c in ['=', ':', ';', '<', '>', '(', ')']:
+                    self.push()
+                    self.buf = c
+                    self.push()
+                else:
+                    self.buf += c
+            elif m == 100: # +
+                if c == '+':
+                    self.buf = '++'
+                    self.push()
+                    m = 0
+                else:
+                    self.stream.prev()
+                    self.buf = '+'
+                    self.push()
+                    m = 0
+
+        self.push()
+
