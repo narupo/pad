5de191cd56614963c9db93d0d252308e3a0a4b2e
narupo <narupo.glab@gmail.com>
2019-11-05 04:08:48 +0900

    imp snippet command for lang

--- src/snippet/snippet.c
+++ src/snippet/snippet.c
@@ -1,15 +1,6 @@
 #include "snippet/snippet.h"
 
 /**
- * Structure of options
- */
-struct opts {
-    bool is_help;
-    bool is_add;
-    bool is_clear;
-};
-
-/**
  * Structure of command
  */
 struct snptcmd {
@@ -17,7 +8,6 @@ struct snptcmd {
     int argc;
     int optind;
     char **argv;
-    struct opts opts;
 };
 
 /**
@@ -33,70 +23,16 @@ snptcmd_show_usage(snptcmd_t *self) {
         "\n"
         "Usage:\n"
         "\n"
-        "    cap snippet [name] [options...]\n"
+        "    cap snippet [name] [commands] [arguments]\n"
         "\n"
-        "The options are:\n"
+        "The commands are:\n"
         "\n"
-        "    -h, --help    show usage\n"
-        "    -a, --add     add snippet code\n"
-        "    -c, --clear   clear all snippet codes\n"
+        "    add     add snippet code\n"
+        "    ls      show list of snippets\n"
+        "    clear   clear all snippet codes\n"
         "\n"
     );
     fflush(stderr);
-}
-
-/**
- * Parse options
- *
- * @param[in] self pointer to snptcmd_t 
- *
- * @return success to true
- * @return failed to false
- */
-static bool
-snptcmd_parse_opts(snptcmd_t *self) {
-    // parse options
-    static struct option longopts[] = {
-        {"help", no_argument, 0, 'h'},
-        {"add", no_argument, 0, 'a'},
-        {"clear", no_argument, 0, 'c'},
-        {0},
-    };
-
-    self->opts = (struct opts){0};
-
-    extern int opterr;
-    extern int optind;
-    opterr = 0; // ignore error messages
-    optind = 0; // init index of parse
-
-    for (;;) {
-        int optsindex;
-        int cur = getopt_long(self->argc, self->argv, "hac", longopts, &optsindex);
-        if (cur == -1) {
-            break;
-        }
-
-        switch (cur) {
-        case 0: /* long option only */ break;
-        case 'h': self->opts.is_help = true; break;
-        case 'a': self->opts.is_add = true; break;
-        case 'c': self->opts.is_clear = true; break;
-        case '?':
-        default:
-            err_die("unknown option");
-            return false;
-            break;
-        }
-    }
-
-    if (self->argc < optind) {
-        err_die("failed to parse option");
-        return false;
-    }
-
-    self->optind = optind;
-    return true;
 }
 
 void
@@ -116,11 +52,6 @@ snptcmd_new(config_t *move_config, int argc, char **move_argv) {
     self->config = move_config;
     self->argc = argc;
     self->argv = move_argv;
-
-    if (!snptcmd_parse_opts(self)) {
-        snptcmd_del(self);
-        return NULL;
-    }
 
     return self;
 }
@@ -148,7 +79,12 @@ snptcmd_show_files(snptcmd_t *self) {
 
 static int
 snptcmd_add(snptcmd_t *self) {
-    const char *name = self->argv[self->optind];
+    if (self->argc < 3) {
+        err_error("failed to add snippet. need file name");
+        return 1;
+    }
+
+    const char *name = self->argv[2];
     char path[FILE_NPATH];
 
     if (!file_solvefmt(path, sizeof path, "%s/%s", self->config->codes_dir_path, name)) {
@@ -173,7 +109,12 @@ snptcmd_add(snptcmd_t *self) {
 
 static int 
 snptcmd_show(snptcmd_t *self) {
-    const char *name = self->argv[self->optind];
+    if (self->argc < 2) {
+        err_error("failed to show snippet. need file name");
+        return 1;
+    }
+
+    const char *name = self->argv[1];
     char path[FILE_NPATH];
 
     if (!file_solvefmt(path, sizeof path, "%s/%s", self->config->codes_dir_path, name)) {
@@ -199,6 +140,11 @@ snptcmd_show(snptcmd_t *self) {
     ast_t *ast = ast_new();
     context_t *ctx = ctx_new();
     opts_t *opts = opts_new();
+
+    if (!opts_parse(opts, self->argc-2, self->argv+2)) {
+        err_error("failed to show snippet. failed to parse options");
+        return 1;
+    }
 
     tkr_parse(tkr, content);
     if (tkr_has_error(tkr)) {
@@ -266,14 +212,14 @@ fail:
 
 int
 snptcmd_run(snptcmd_t *self) {
-    if (self->opts.is_help) {
+    if (self->argc < 2) {
         snptcmd_show_usage(self);
         return 0;
-    } else if (self->opts.is_clear) {
+    } else if (cstr_eq(self->argv[1], "clear")) {
         return snptcmd_clear(self);
-    } else if (self->argc < self->optind+1) {
+    } else if (cstr_eq(self->argv[1], "ls")) {
         return snptcmd_show_files(self);
-    } else if (self->opts.is_add) {
+    } else if (cstr_eq(self->argv[1], "add")) {
         return snptcmd_add(self);
     }
     return snptcmd_show(self);
