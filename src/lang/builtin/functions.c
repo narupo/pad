a86546558d563a4ed9b75ba4a95cc2fdd38bda4a
narupo <narupo.glab@gmail.com>
2020-02-22 00:22:51 +0900

    refactoring for string function of builtin functions

--- src/lang/builtin/functions.c
+++ src/lang/builtin/functions.c
@@ -77,7 +77,7 @@ builtin_exec(ast_t *ast, const object_t *drtargs) {
 }
 
 static object_t *
-builtin_lower(ast_t *ast, const object_t *_) {
+call_basic_str_func(ast_t *ast, const char *method_name) {
     const object_t *owner = ast->ref_dot_owner;
     if (!owner) {
         return obj_new_nil();
@@ -87,121 +87,57 @@ builtin_lower(ast_t *ast, const object_t *_) {
 again:
     switch (owner->type) {
     default:
-        ast_set_error_detail(ast, "can't call lower function");
+        ast_set_error_detail(ast, "can't call %s function", method_name);
         return NULL;
         break;
     case OBJ_TYPE_STRING: {
-        string_t *str = str_lower(owner->string);
-        return obj_new_str(str);
+        string_t *result = NULL;
+        if (cstr_eq(method_name, "lower")) {
+            result = str_lower(owner->string);
+        } else if (cstr_eq(method_name, "upper")) {
+            result = str_upper(owner->string);
+        } else if (cstr_eq(method_name, "capitalize")) {
+            result = str_capitalize(owner->string);
+        } else if (cstr_eq(method_name, "snake")) {
+            result = str_snake(owner->string);
+        } else {
+            ast_set_error_detail(ast, "invalid method name \"%s\" for call basic string function", method_name);
+            return NULL;
+        }
+        return obj_new_str(result);
     } break;
     case OBJ_TYPE_IDENTIFIER: {
         owner = ctx_find_var_ref(ast->context, str_getc(owner->identifier));
         if (!owner) {
-            ast_set_error_detail(ast, "not found \"%s\" in lower function", owner->identifier);
+            ast_set_error_detail(ast, "not found \"%s\" in %s function", owner->identifier, method_name);
             return NULL;
         }
         goto again;
     } break;
     }
 
-    assert(0 && "impossible. failed to invoke lower function");
+    assert(0 && "impossible. failed to invoke basic string function");
     return obj_new_nil();
+}
+
+static object_t *
+builtin_lower(ast_t *ast, const object_t *_) {
+    return call_basic_str_func(ast, "lower");
 }
 
 static object_t *
 builtin_upper(ast_t *ast, const object_t *_) {
-    const object_t *owner = ast->ref_dot_owner;
-    if (!owner) {
-        return obj_new_nil();
-    }
-    ast->ref_dot_owner = NULL;
-
-again:
-    switch (owner->type) {
-    default:
-        ast_set_error_detail(ast, "can't call upper function");
-        return NULL;
-        break;
-    case OBJ_TYPE_STRING: {
-        string_t *str = str_upper(owner->string);
-        return obj_new_str(str);
-    } break;
-    case OBJ_TYPE_IDENTIFIER: {
-        owner = ctx_find_var_ref(ast->context, str_getc(owner->identifier));
-        if (!owner) {
-            ast_set_error_detail(ast, "not found \"%s\" in upper function", owner->identifier);
-            return NULL;
-        }
-        goto again;
-    } break;
-    }
-
-    assert(0 && "impossible. failed to invoke upper function");
-    return obj_new_nil();
+    return call_basic_str_func(ast, "upper");
 }
 
 static object_t *
 builtin_capitalize(ast_t *ast, const object_t *_) {
-    const object_t *owner = ast->ref_dot_owner;
-    if (!owner) {
-        return obj_new_nil();
-    }
-    ast->ref_dot_owner = NULL;
-
-again:
-    switch (owner->type) {
-    default:
-        ast_set_error_detail(ast, "can't call capitalize function");
-        return NULL;
-        break;
-    case OBJ_TYPE_STRING: {
-        string_t *str = str_capitalize(owner->string);
-        return obj_new_str(str);
-    } break;
-    case OBJ_TYPE_IDENTIFIER: {
-        owner = ctx_find_var_ref(ast->context, str_getc(owner->identifier));
-        if (!owner) {
-            ast_set_error_detail(ast, "not found \"%s\" in capitalize function", owner->identifier);
-            return NULL;
-        }
-        goto again;
-    } break;
-    }
-
-    assert(0 && "impossible. failed to invoke capitalize function");
-    return obj_new_nil();
+    return call_basic_str_func(ast, "capitalize");
 }
 
 static object_t *
 builtin_snake(ast_t *ast, const object_t *_) {
-    const object_t *owner = ast->ref_dot_owner;
-    if (!owner) {
-        return obj_new_nil();
-    }
-    ast->ref_dot_owner = NULL;
-
-again:
-    switch (owner->type) {
-    default:
-        ast_set_error_detail(ast, "can't call snake function");
-        return NULL;
-        break;
-    case OBJ_TYPE_STRING: {
-        string_t *str = str_snake(owner->string);
-        return obj_new_str(str);
-    } break;
-    case OBJ_TYPE_IDENTIFIER: {
-        owner = ctx_find_var_ref(ast->context, str_getc(owner->identifier));
-        if (!owner) {
-            ast_set_error_detail(ast, "not found \"%s\" in snake function", owner->identifier);
-            return NULL;
-        }
-        goto again;
-    } break;
-    }
-
-    assert(0 && "impossible. failed to invoke snake function");
-    return obj_new_nil();
+    return call_basic_str_func(ast, "snake");
 }
 
 static builtin_func_info_t
