2288a02cf44a783412887956c4f5b0086610bebc
narupo <narupo.glab@gmail.com>
2020-02-25 23:44:29 +0900

    rm lang/.py/

--- src/lang/.py/context.py
+++
@@ -1,7 +0,0 @@
-class Context:
-    def __init__(self):
-        self.buffer = ''
-        self.imported_alias = False
-        self.alias_map = {}
-        self.last_expr_val = 0
-        self.func_context = None # FuncContext
--- src/lang/.py/tokens.py
+++
@@ -1,7 +0,0 @@
-class Token:
-    def __init__(self, kind='', value=''):
-        self.kind = kind
-        self.value = value
-
-    def __str__(self):
-        return '<{0},{1}>'.format(self.kind, self.value)
