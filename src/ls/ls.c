54c08259fb4a33b8c1ae13aa3d113ed2c61e04bc
narupo <narupo.glab@gmail.com>
2020-04-12 18:45:39 +0900

    display color smylink file

--- src/ls/ls.c
+++ src/ls/ls.c
@@ -105,6 +105,8 @@ print_fname(const lscmd_t *self, FILE *fout, bool print_color, const char *path,
     
     if (file_isdir(fpath)) {
         term_cfprintf(fout, TERM_WHITE, TERM_GREEN, TERM_BRIGHT, "%s\n", name);
+    } else if (symlink_is_link_file(fpath)) {
+        term_cfprintf(fout, TERM_CYAN, TERM_BLACK, TERM_BRIGHT, "%s\n", name);
     } else {
         term_cfprintf(fout, TERM_GREEN, TERM_BLACK, TERM_BRIGHT, "%s\n", name);
     }
