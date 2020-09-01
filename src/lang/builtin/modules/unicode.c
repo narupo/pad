107ee4c2b3a7b54cbe1fa3b7c73bfb55898a4d83
narupo <narupo.glab@gmail.com>
2020-09-01 15:06:58 +0900

    refactoring for builtin unicode module

--- src/lang/builtin/modules/unicode.c
+++ src/lang/builtin/modules/unicode.c
@@ -1,12 +1,11 @@
 #include <lang/builtin/modules/unicode.h>
 
 static object_t *
-call_basic_unicode_func(const char *method_name, builtin_func_args_t *fargs) {
-    ast_t *ref_ast = fargs->ref_ast;
-    assert(ref_ast);
-    object_array_t *ref_owners = fargs->ref_owners;
-    assert(ref_owners);
-
+extract_unicode_object(ast_t *ref_ast, object_array_t *ref_owners, const char *method_name) {
+    if (!ref_ast || !ref_owners || !method_name) {
+        return NULL;
+    }
+    
     object_t *owner = objarr_get_last(ref_owners);
     if (!owner) {
         return obj_new_nil(ref_ast->ref_gc);
@@ -19,24 +18,7 @@ again:
         return NULL;
         break;
     case OBJ_TYPE_UNICODE: {
-        unicode_t *result = NULL;
-        if (cstr_eq(method_name, "lower")) {
-            result = uni_lower(owner->unicode);
-        } else if (cstr_eq(method_name, "upper")) {
-            result = uni_upper(owner->unicode);
-        } else if (cstr_eq(method_name, "capitalize")) {
-            result = uni_capitalize(owner->unicode);
-        } else if (cstr_eq(method_name, "snake")) {
-            result = uni_snake(owner->unicode);
-        } else if (cstr_eq(method_name, "camel")) {
-            result = uni_camel(owner->unicode);
-        } else if (cstr_eq(method_name, "hacker")) {
-            result = uni_hacker(owner->unicode);
-        } else {
-            ast_pushb_error(ref_ast, "invalid method name \"%s\" for call basic unicode method", method_name);
-            return NULL;
-        }
-        return obj_new_unicode(ref_ast->ref_gc, result);
+        return owner;
     } break;
     case OBJ_TYPE_IDENTIFIER: {
         owner = ctx_find_var_ref(ref_ast->ref_context, obj_getc_idn_name(owner));
@@ -61,101 +43,112 @@ again:
 }
 
 static object_t *
-builtin_unicode_lower(builtin_func_args_t *args) {
-    return call_basic_unicode_func("lower", args);
+call_basic_unicode_func(const char *method_name, builtin_func_args_t *fargs) {
+    if (!method_name || !fargs) {
+        return NULL;
+    }
+
+    object_t *owner = extract_unicode_object(
+        fargs->ref_ast,
+        fargs->ref_owners,
+        method_name
+    );
+    if (!owner) {
+        ast_pushb_error(fargs->ref_ast, "failed to extract unicode object");
+        return NULL;
+    }
+
+    unicode_t *result = NULL;
+    if (cstr_eq(method_name, "lower")) {
+        result = uni_lower(owner->unicode);
+    } else if (cstr_eq(method_name, "upper")) {
+        result = uni_upper(owner->unicode);
+    } else if (cstr_eq(method_name, "capitalize")) {
+        result = uni_capitalize(owner->unicode);
+    } else if (cstr_eq(method_name, "snake")) {
+        result = uni_snake(owner->unicode);
+    } else if (cstr_eq(method_name, "camel")) {
+        result = uni_camel(owner->unicode);
+    } else if (cstr_eq(method_name, "hacker")) {
+        result = uni_hacker(owner->unicode);
+    } else {
+        ast_pushb_error(fargs->ref_ast, "invalid method name \"%s\" for call basic unicode method", method_name);
+        return NULL;
+    }
+
+    return obj_new_unicode(fargs->ref_ast->ref_gc, result);
 }
 
 static object_t *
-builtin_unicode_upper(builtin_func_args_t *args) {
-    return call_basic_unicode_func("upper", args);
+builtin_unicode_lower(builtin_func_args_t *fargs) {
+    return call_basic_unicode_func("lower", fargs);
 }
 
 static object_t *
-builtin_unicode_capitalize(builtin_func_args_t *args) {
-    return call_basic_unicode_func("capitalize", args);
+builtin_unicode_upper(builtin_func_args_t *fargs) {
+    return call_basic_unicode_func("upper", fargs);
 }
 
 static object_t *
-builtin_unicode_snake(builtin_func_args_t *args) {
-    return call_basic_unicode_func("snake", args);
+builtin_unicode_capitalize(builtin_func_args_t *fargs) {
+    return call_basic_unicode_func("capitalize", fargs);
 }
 
 static object_t *
-builtin_unicode_camel(builtin_func_args_t *args) {
-    return call_basic_unicode_func("camel", args);
+builtin_unicode_snake(builtin_func_args_t *fargs) {
+    return call_basic_unicode_func("snake", fargs);
 }
 
 static object_t *
-builtin_unicode_hacker(builtin_func_args_t *args) {
-    return call_basic_unicode_func("hacker", args);
+builtin_unicode_camel(builtin_func_args_t *fargs) {
+    return call_basic_unicode_func("camel", fargs);
+}
+
+static object_t *
+builtin_unicode_hacker(builtin_func_args_t *fargs) {
+    return call_basic_unicode_func("hacker", fargs);
 }
 
 static object_t *
 builtin_unicode_split(builtin_func_args_t *fargs) {
-    ast_t *ref_ast = fargs->ref_ast;
-    assert(ref_ast);
-    object_array_t *ref_owners = fargs->ref_owners;
-    assert(ref_owners);
-    object_t *ref_args = fargs->ref_args;
-    assert(ref_args);
-    object_array_t *args = ref_args->objarr;
+    if (!fargs) {
+        return NULL;
+    }
+
+    object_array_t *args = fargs->ref_args->objarr;
     assert(args);
     const object_t *sep = objarr_getc(args, 0);
     if (sep->type != OBJ_TYPE_UNICODE) {
-        ast_pushb_error(ref_ast, "invalid argument");
+        ast_pushb_error(fargs->ref_ast, "invalid argument");
         return NULL;
     }
     const unicode_type_t *unisep = uni_getc(sep->unicode);
-    const char *method_name = "split";
 
-    object_t *owner = objarr_get_last(ref_owners);
+    object_t *owner = extract_unicode_object(
+        fargs->ref_ast,
+        fargs->ref_owners,
+        "split"
+    );
     if (!owner) {
-        return obj_new_nil(ref_ast->ref_gc);
-    }
-
-again:
-    switch (owner->type) {
-    default:
-        ast_pushb_error(ref_ast, "can't call %s method", method_name);
+        ast_pushb_error(fargs->ref_ast, "failed to extract unicode object");
         return NULL;
-        break;
-    case OBJ_TYPE_UNICODE: {
-        unicode_t ** arr = uni_split(owner->unicode, unisep);
-        if (!arr) {
-            ast_pushb_error(ref_ast, "failed to split");
-            return NULL;
-        }
-
-        object_array_t *toks = objarr_new();
-        for (unicode_t **p = arr; *p; ++p) {
-            object_t *obj = obj_new_unicode(ref_ast->ref_gc, mem_move(*p));
-            objarr_moveb(toks, mem_move(obj));
-        }
-        free(arr);
-
-        object_t *ret = obj_new_array(ref_ast->ref_gc, mem_move(toks));
-        return ret;
-    } break;
-    case OBJ_TYPE_IDENTIFIER: {
-        owner = ctx_find_var_ref(ref_ast->ref_context, obj_getc_idn_name(owner));
-        if (!owner) {
-            ast_pushb_error(ref_ast, "not found \"%s\" in %s method", owner->identifier, method_name);
-            return NULL;
-        }
-        goto again;
-    } break;
-    case OBJ_TYPE_CHAIN: {
-        owner = refer_chain_obj_with_ref(ref_ast, owner);
-        if (!owner) {
-            ast_pushb_error(ref_ast, "failed to refer index");
-            return NULL;
-        }
-        goto again;
-    } break;
     }
 
-    assert(0 && "impossible. failed to invoke basic unicode function");
-    return obj_new_nil(ref_ast->ref_gc);
+    unicode_t ** arr = uni_split(owner->unicode, unisep);
+    if (!arr) {
+        ast_pushb_error(fargs->ref_ast, "failed to split");
+        return NULL;
+    }
+
+    object_array_t *toks = objarr_new();
+    for (unicode_t **p = arr; *p; ++p) {
+        object_t *obj = obj_new_unicode(fargs->ref_ast->ref_gc, mem_move(*p));
+        objarr_moveb(toks, mem_move(obj));
+    }
+    free(arr);
+
+    object_t *ret = obj_new_array(fargs->ref_ast->ref_gc, mem_move(toks));
+    return ret;
 }
 
 static builtin_func_info_t
